9cdf1eef2214c3e3fa366dc1710470f1e479e845
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2024-04-11 18:45:08 +0300

    Add files via upload

---
+++ 26,03/26,03/26,03.cpp
@@ -0,0 +1,58 @@
+﻿//Перше завдання
+/*#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Point {
+public:
+    int x, y;
+    Point(int _x, int _y) : x(_x), y(_y) {}
+};
+
+int main() {
+    vector<Point> points;
+
+    points.push_back(Point(1, 2));
+    points.push_back(Point(3, 4));
+    points.push_back(Point(5, 6));
+    points.push_back(Point(7, 8));
+    points.push_back(Point(9, 10));
+
+    cout << "Елементи вектора Point:" << endl;
+    for (const Point& p : points) {
+        cout << "x: " << p.x << ", y: " << p.y << endl;
+    }
+
+    return 0;
+}
+*/
+//Друге завдання
+/*#include <iostream>
+#include <list>
+
+using namespace std;
+
+class Point {
+public:
+    int x, y;
+    Point(int _x, int _y) : x(_x), y(_y) {}
+};
+
+int main() {
+    list<Point> points;
+
+    points.push_back(Point(1, 2));
+    points.push_back(Point(3, 4));
+    points.push_back(Point(5, 6));
+    points.push_back(Point(7, 8));
+    points.push_back(Point(9, 10));
+
+    cout << "Елементи списку Point:" << endl;
+    for (const Point& p : points) {
+        cout << "x: " << p.x << ", y: " << p.y << endl;
+    }
+
+    return 0;
+}
+*/
